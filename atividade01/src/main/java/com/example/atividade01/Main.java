ba4527a193c9843bff399be08701579f53bfc2d0
Johnatan Rodrigues <johndriguess@gmail.com>
2025-09-01 11:58:04 -0300

    threads

--- atividade01/src/main/java/com/example/atividade01/Main.java
+++ atividade01/src/main/java/com/example/atividade01/Main.java
@@ -12,16 +12,16 @@ public class Main {
         Hypervisor hypervisor = new Hypervisor();
 
         VirtualMachine vm1 = new VirtualMachine("VM1", hypervisor);
+        vm1.setMessage("Hello from network to VM1");
+
         VirtualMachine vm2 = new VirtualMachine("VM2", hypervisor);
+        vm2.setMessage("Ping request to VM2");
 
         // Adicionar ao hipervisor
         hypervisor.addVM(vm1);
         hypervisor.addVM(vm2);
 
-        System.out.println("--------------------------------------------------");
-        vm1.getNetworkCard().receiveData("Hello from network to VM1");
-
-        System.out.println("--------------------------------------------------");
-        vm2.getNetworkCard().receiveData("Ping request to VM2");
+        // Iniciar todas as VMs
+        hypervisor.startAllVMs();
     }
 }
